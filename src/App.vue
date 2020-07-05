05e092959fb41b97629e1449f85c833e66eccdf4
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-07-05 12:20:47 -0300

    navegando entre rotas

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
       </div>
 
       <div class="container">
+
+        <router-link to="/" class="btn btn-info mb-2 mr-2">Home</router-link>
+        <router-link :to="{ path: '/contatos'}" class="btn btn-info mb-2">Contatos</router-link>
+        
         <router-view></router-view>
       </div>
   </div>
