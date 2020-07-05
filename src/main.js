ba4a78eaf7ebad032eab6b84dea51be03b4bb05d
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-07-05 12:23:59 -0300

    exportando arquivo de configuracoes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-import Contatos from './views/contatos/Contatos.vue';
-import Home from './views/Home.vue';
+import router from './router.js'
+
 
 Vue.config.productionTip = false
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    { path: '/contatos', component: Contatos },
-    { path: '/', component: Home },
-  ]
-});
 
 new Vue({
   router: router,
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Contatos from './views/contatos/Contatos.vue';
+import Home from './views/Home.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/contatos', component: Contatos },
+    { path: '/', component: Home },
+  ]
+});
