54e7a33ef280dda0d94863c75982a6e4da777172
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-07-05 12:39:44 -0300

    navegando de forma programatica

--- src/views/contatos/Contatos.vue
+++ src/views/contatos/Contatos.vue
@@ -1,3 +1,24 @@
 <template>
-    <h3 class="font-weight-light">Contatos</h3>
+    <div>
+        <h3 class="font-weight-light">Contatos</h3>
+        <button 
+            class="btn btn-secondary mt-4 mb-4"
+            @click="voltar">
+            Voltar
+        </button>
+    </div>            
 </template>
+
+<script>
+export default {
+    methods: {
+        voltar() {
+            // this.$router.push('/')
+            // this.$router.replace('/');
+            // this.$router.go(-1);
+            // this.$router.forward();
+            this.$router.back();
+        }
+    }
+}
+</script>
